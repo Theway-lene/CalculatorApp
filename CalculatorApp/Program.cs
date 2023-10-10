b16500ae9c40a1840bdbdce4cc20e8065dead4ce
hp <hp@DANIEL-DAUDU>
2023-10-10 14:18:01 +0100

    Add project files.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+class calculatorApp
+{
+    static void Main()
+    {
+        Console.WriteLine("-----------------");
+        Console.WriteLine("Start Calculating");
+        Console.WriteLine("-----------------");
+        Console.WriteLine("Type 'c' as an operation to clear");
+        Console.WriteLine("-----------------");
+
+        double num1 = 0;
+        double num2;
+        double result = 0;
+        int whileTrigger = 0;
+
+        Console.Write("Enter first number: ");
+        num1 = double.Parse(Console.ReadLine());
+        string operatn = "";
+        do
+        {
+
+            Console.Write("Enter Operation: ");
+            operatn = (Console.ReadLine());
+
+            switch (operatn)
+            {
+                case "+":
+                    Console.Write("Enter second Number: ");
+                    num2 = double.Parse(Console.ReadLine());
+                    result = num1 + num2;
+                    Console.WriteLine($"{num1} {operatn} {num2} = {result}");
+                    num1 = result;
+                    break;
+
+                case "-":
+                    Console.Write("Enter second Number: ");
+                    num2 = double.Parse(Console.ReadLine());
+                    result = num1 - num2;
+                    Console.WriteLine($"{num1} {operatn} {num2} = {result}");
+                    num1 = result;
+                    break;
+
+                case "*":
+                    Console.Write("Enter second Number: ");
+                    num2 = double.Parse(Console.ReadLine());
+                    result = num1 * num2;
+                    Console.WriteLine($"{num1} {operatn} {num2} = {result}");
+                    num1 = result;
+                    break;
+
+                case "/":
+                    Console.Write("Enter second Number: ");
+                    num2 = double.Parse(Console.ReadLine());
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine($"{num1} {operatn} {num2} = {result}");
+                        num1 = result;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid");
+                    }    
+                    break;
+
+                case "c":
+
+                    num1 = 0;
+                    Console.WriteLine(num1);
+                    Console.Write("Enter first number: ");
+                    num1 = double.Parse(Console.ReadLine());
+
+                    Console.Write("Enter Operation: ");
+                    operatn = (Console.ReadLine());
+
+                    Console.Write("Enter second Number: ");
+                    num2 = double.Parse(Console.ReadLine());
+                    result = num1 + num2;
+                    Console.WriteLine($"{num1} {operatn} {num2} = {result}");
+                    num1 = result;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+        }
+        while (whileTrigger == 0);
+
+    }
+}
